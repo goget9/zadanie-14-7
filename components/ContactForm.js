a7cf23a964d6d35e42bd4d2a2630dbbc6a8e1ea9
Woji <lubowicki.w@gmail.com>
2018-03-23 14:12:30 +0100

    Issues with components import/export

---
+++ components/ContactForm.js
@@ -0,0 +1,19 @@
+var ContactForm = React.createClass({
+  propTypes: {
+    contact: React.PropTypes.object.isRequired
+  },
+
+  render: function() {
+    return (
+      <form className={'contactForm'}>
+        <input type={'text'} placeholder={'Name'} value={this.props.contact.firstName}/>
+        <input type={'text'} placeholder={'Last Name'} value={this.props.contact.lastName}/>
+        <input type={'email'} placeholder={'Email'} value={this.props.contact.email}/>
+        <input type={'text'} placeholder={'Name'} value={this.props.contact.firstName}/>
+        <button type={'submit'}>Add Contact</button>
+      </form>
+    )
+  },
+});
+
+export default ContactForm
---
+++ script.js
@@ -0,0 +1,5 @@
+import App from './components/App.js'
+
+
+var app = React.createElement(App);
+ReactDOM.render(app, document.getElementById('app'));
